ab78f464a4274c8e976cf520d6b0892c1144df19
dwrolvink <dwrolvink@gmail.com>
2023-05-25 17:27:33 +0200

    add module options (part 2)

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,18 @@ use obshtml::{ObsidianModuleConfig, ObsidianModule};
 use obshtml::module::options::{compile_default_options}; //get_configured_options
 use obshtml::cli::execute;
 
+use obshtml::stdlib::*;
+
 fn main() {
     // define the default config options for this module that can be overwritten
     // by users in module_config: {}
-    let default_options = compile_default_options("setting1: bla");
+    let default_options = compile_default_options("
+a:
+    a1: old (should be overwritten)
+    a2: old (should not be overwritten)
+b: old (should be overwritten)
+c: old (only in default)
+        ");
 
     // define module config
     let obs_cfg = ObsidianModuleConfig {
@@ -28,9 +36,18 @@ fn main() {
 }
 
 fn run(obsmod: ObsidianModule) {
-    // get module options example
-    let setting1 = obsmod.default_options["setting1"].as_str().unwrap();
-    println!("{:?}", setting1);
+    // example of how to use options and the types of the
+    // returnded values
+    // note the eprintln, we should only print valid json to stdout
+    let val = &obsmod.options["a"];
+    eprintln!("Debug: {}< {:?} >", get_type_of(val), val, );
+
+    let val = &obsmod.options["b"];
+    eprintln!("Debug: {}< {:?} >", get_type_of(val), val, );
+
+    let val = &obsmod.options["b"].as_str().unwrap();
+    eprintln!("Debug: {}< {:?} >", get_type_of(val), val, );
+
 
     // return output
     // make sure to only output valid json to stdout when running as an actual module
