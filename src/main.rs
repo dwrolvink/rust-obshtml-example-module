e40546f52daeef34a8a2b9f3b05c7ef75c5d11e3
dwrolvink <dwrolvink@gmail.com>
2023-05-26 14:19:32 +0200

    introduce modfile

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,10 @@ c: old (only in default)
 }
 
 fn run(obsmod: ObsidianModule) {
-    // example of how to use options and the types of the
-    // returnded values
-    // note the eprintln, we should only print valid json to stdout
+
+    // example of how to get the module options
+    // it also shows the returned values and how to unpack them
+    // *note also the eprintln, we should only print valid json to stdout!*
     let val = &obsmod.options["a"];
     eprintln!("Debug: {}< {:?} >", get_type_of(val), val, );
 
@@ -48,6 +49,9 @@ fn run(obsmod: ObsidianModule) {
     let val = &obsmod.options["b"].as_str().unwrap();
     eprintln!("Debug: {}< {:?} >", get_type_of(val), val, );
 
+    // read a random modfile
+    let mod_file = obsmod.modfile("paths.json");
+    eprintln!("{}", mod_file.read().unwrap());
 
     // return output
     // make sure to only output valid json to stdout when running as an actual module
