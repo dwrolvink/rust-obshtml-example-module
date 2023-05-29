58e851b3e05b9cbbe987ba18750ebaf5e8a47f21
dwrolvink <dwrolvink@gmail.com>
2023-05-29 03:39:42 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
-//#[macro_use(out)]
 extern crate obshtml;
 extern crate yaml_rust;
 extern crate json;
 extern crate regex;
+extern crate linked_hash_map;
 
 #[macro_use]
 extern crate lazy_static;
 
-extern crate linked_hash_map;
-
-use std::path::{Path};
-
-use json::JsonValue;
-
-//use yaml_rust::{YamlEmitter}; //YamlLoader
-use yaml_rust::Yaml;
-use json::iterators::Members;
+// data structures
 use linked_hash_map::LinkedHashMap;
+use yaml_rust::Yaml;
+use json::JsonValue;
+use json::iterators::Members;
 
+// obshtml elements
 use obshtml::{ObsidianModuleConfig, ObsidianModule};
 use obshtml::module::options::{compile_default_options}; //get_configured_options
 use obshtml::module::modfile::{compile_provides};
 use obshtml::cli::execute;
 
-
+// helper functions/objects
 use obshtml::stdlib::*;
 use obshtml::lib::file;
 use obshtml::lib::misc::{yaml_to_json};
+use std::path::{Path};
 
+// internal modules
 mod metadata;
 use metadata::{
     parse_frontmatter, 
@@ -40,21 +38,17 @@ use metadata::{
 fn main() {
     // define the default config options for this module that can be overwritten
     // by users in module_config: {}
-    let default_options = compile_default_options("
-a:
-    a1: old (should be overwritten)
-    a2: old (should not be overwritten)
-b: old (should be overwritten)
-c: old (only in default)
-        ");
+    let default_options = compile_default_options("test: set this value to test options being set");
 
-    let provides = compile_provides(vec!("metadata.json"));
+    // list files that this module will create/alter
+    let provides = compile_provides(vec!("index/metadata.json"));
+
+    // TODO: add requires!
 
     // define module config
     let obs_cfg = ObsidianModuleConfig {
         module_name: "hello",
         module_class_name: "<crate::obshtml-example>",
-        persistent: false,
         run_fn: run,
         accept_fn: accept,
         default_options: default_options,
@@ -65,18 +59,33 @@ c: old (only in default)
 }
 
 fn run(obsmod: ObsidianModule) {
-    // get files.json (we need the input folder later on)
+    /*
+        This function is the "meat" of this module. 
+        It will get the modfile index/files.json, and for each markdown file parse the frontmatter yaml 
+        and get the inline tags.
+        It then merges the tags listed in the frontmatter, and the found inline tags, removing duplicates,
+        making sure that the metadata["tags"] key is set with an empty list if no tags are found at all.
+
+        The result is written to modfile index/metadata.json with structure:
+        {
+            "$REL_PATH": {$METADATA},
+            "$REL_PATH": {$METADATA},
+            ...
+        }
+    */
+
+    // Get paths.json and then get paths["input_folder"]
+    // We need this value to get the rel_path of the files (index/files.json has absolute paths)
     let paths_modfile = obsmod.modfile("paths.json");
     let contents = paths_modfile.read().unwrap();
     let paths = json::parse(&contents).unwrap();
     let input_folder_path = paths["input_folder"].as_str().unwrap();
-    println!("{:?}", input_folder_path);
 
-    // we will add all the metadata into this hashtable
-    // using the rel_path as the key, and the metadata as the value
+    // We will add all the metadata into this hashtable as we collect it.
+    // Using the rel_path as the key, and the metadata as the value
     let mut output_hash = json::JsonValue::new_object();
 
-    // read index/files.json and read the file paths in the list
+    // Read index/files.json and read the file paths in the list
     let mod_file = obsmod.modfile("index/files.json");
     obsmod.stderr("debug", &format!("abs path of modfile: {}", &mod_file.get_abs_file_path()));
 
@@ -87,13 +96,11 @@ fn run(obsmod: ObsidianModule) {
 
         // get the frontmatter from markdown files
         if file_path.ends_with(".md") {
-            obsmod.stderr("debug", &format!("getting frontmatter for: {}", file_path));
+            obsmod.stderr("debug", &format!("getting metadata for: {}", file_path));
 
             let mut frontmatter = parse_frontmatter(&obsmod, file_path);
             ensure_tags_item_in_frontmatter(&mut frontmatter);
             convert_tags_from_string_to_list(&mut frontmatter);
-
-            obsmod.stderr("debug", &format!("    {:?}", frontmatter));
 
             let contents = file::read(file_path).unwrap();
             let inline_tags = get_inline_tags(&contents);
@@ -112,15 +119,19 @@ fn run(obsmod: ObsidianModule) {
                 _ => (),
             }
 
-            // get rel_path
+            // this is our final frontmatter
+            obsmod.stderr("debug", &format!("    {:?}", frontmatter));
+
+            // get rel_path and write to output_hash
             let abs_path = Path::new(file_path);
             let rel_path_res = abs_path.strip_prefix(input_folder_path);
             match rel_path_res {
                 Err(inner) => {
+                    // failed to get rel_path
                         obsmod.stderr("error", &format!("Failed to get relative path for rel({}, {}), first is not the parent of the second", input_folder_path, file_path));
                     },
                 Ok(rel_path) => {
-                    // write to output hash
+                    // we got the rel_path, now we can write the metadata to the output_hash
                     let rel_path_str = rel_path.to_str().unwrap().to_owned();
                     output_hash[rel_path_str] = yaml_to_json(&frontmatter).unwrap();
                 },
@@ -128,19 +139,10 @@ fn run(obsmod: ObsidianModule) {
         }
     }
 
-    // println!("{:?}", output_hash);
-    // let mut out_yaml_str = String::new();
-    // let mut emitter = YamlEmitter::new(&mut out_yaml_str);
-    // emitter.dump(&Yaml::Hash(output_hash)).unwrap(); 
-
-
-    // write a output modfile
-    let out_mod_file = obsmod.modfile("metadata.json");
+    // write the output modfile
+    let out_mod_file = obsmod.modfile("index/metadata.json");
     out_mod_file.write(&output_hash.pretty(2)).unwrap();
 
-    // obsmod.stderr("debug", &format!("{}< {:?} >", get_type_of(&it), it));
-    //println!("{:?}", file_paths);
-    
     // return output
     // make sure to only output valid json to stdout when running as an actual module
     let output = r#"{"result": true}"#;
@@ -149,7 +151,10 @@ fn run(obsmod: ObsidianModule) {
 
 
 fn accept(_obsmod: ObsidianModule) {
-    //println!("inside accept_fn:\n\tmodule_data_folder: {}", module_data_folder);
+    /*
+        This function is called by ObsidianHtml to test if we need to run the module proper
+        In our current case, we always want to run this module when configured
+    */
     let output = r#"{"result": true}"#;
     println!("{}", output);
 }
